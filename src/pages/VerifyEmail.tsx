b7057ca8a6499ebf5e0d22a8fce129ca78b31b4f
Danh Tran <danh2k@nexondv.com>
2024-05-15 17:59:45 +0700

    tmp

--- src/pages/VerifyEmail.tsx
+++ src/pages/VerifyEmail.tsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from 'react'
+import { useLocation, useSearchParams } from 'react-router-dom'
+
 export default function VerifyEmail() {
   //  Thật ra chỉ là 1 loading chờ useEffect run để navigate đi
-  return <div>VerifyEmail</div>
+
+  const [message] = useState('VerifyEmail')
+
+  const loc = useLocation()
+  const [searchParams] = useSearchParams()
+  const paramsObj = Object.fromEntries(searchParams) as {
+    token?: string
+  }
+
+  console.log('🚀 ~ router-dom:', { paramsObj, searchParams, loc })
+
+  useEffect(() => {
+    const controller = new AbortController()
+
+    return () => {
+      controller.abort()
+    }
+  })
+  return <div>{message}</div>
 }
