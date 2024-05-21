efa61af1101e31472c98cff7c6fe494225ef9ed3
Danh Tran <danh2k@nexondv.com>
2024-05-21 07:52:03 +0700

    small refactor socket

--- src/feature/useSocket.ts
+++ src/feature/useSocket.ts
@@ -1,12 +1,30 @@
 import { useEffect } from 'react'
-import { io } from 'socket.io-client'
+import { Socket, io } from 'socket.io-client'
 
-const initSocket = () => io(import.meta.env.VITE_API_URL)
+const initSocket = () =>
+  io(
+    import.meta.env.PROD
+      ? import.meta.env.VITE_API_URL
+      : import.meta.env.VITE_API_URL
+  )
+
+const addSocketListen = (socket: Socket) => {
+  socket.on('connect', () => {
+    console.log(`🚀 ~ user ${socket.id} connect`)
+  })
+
+  socket.on('disconnect', reason => {
+    console.log(`🚀 ~ user ${socket.id} disconnect`)
+    console.log(reason)
+  })
+}
 
 export const useSocket = () => {
   useEffect(() => {
     // https://socket.io/docs/v4/client-initialization/#from-a-different-domain
-    const socket = io('http://localhost:4007')
+    const socket = initSocket()
+    addSocketListen(socket)
+
     return () => {
       socket.disconnect()
     }
