49a0b36a19653804e1f5ab2852d6d816ad4733a8
Danh Tran <danh2k@nexondv.com>
2024-05-24 10:31:43 +0700

    fix: add auth to socket connect

--- src/feature/useSocket.ts
+++ src/feature/useSocket.ts
@@ -4,18 +4,27 @@ import { addSocketListen, socket } from './socketInit'
 // Vì viết dạng singleton nên việc disconnect này sẽ disconnect socket toàn app
 // Tuy nhiên nếu connect cũng chỉ connect vào 1 api socket duy nhất
 
-export const useSocket = () => {
+export const useSocket = (payload?: { [key: string]: unknown }) => {
+  // const token = payload?.['Authorization'] as string
+
   useEffect(() => {
+    // TODO: tạm thời check thử xem dùng obj sẽ ảnh hưởng render ntn
+    console.count('🚀🚀 useSocket L9 run')
+
     // https://socket.io/docs/v4/client-initialization/#from-a-different-domain
     // const socket = initSocket()
     // https://socket.io/how-to/use-with-react#disconnection
+    if (payload) {
+      socket.auth = payload
+    }
+
     socket.connect()
     addSocketListen(socket)
 
     return () => {
       socket.disconnect()
     }
-  }, [])
+  }, [payload])
 
   const reConnectSocket = () => socket.connect()
   const disconnectSocket = () => socket.disconnect()
--- src/pages/Chat2.tsx
+++ src/pages/Chat2.tsx
@@ -1,8 +1,13 @@
 import ChatPanel from '../feature/Chat/ChatPanel/ChatPanel'
 import UserPanel from '../feature/Chat/UserPanel/UserPanel'
+import { useSocket } from '../feature/useSocket'
 import styles from './Chat2.module.css'
 
 export default function Chat2() {
+  const access_token = localStorage.getItem('access_token')
+
+  useSocket({ Authorization: `Bearer ${access_token}` })
+
   return (
     <div className=''>
       <div className={styles['left-panel']}>
